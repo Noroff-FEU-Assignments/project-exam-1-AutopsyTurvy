799b117e5fa5393a4c02a02f43d59609bc1d8f55
AutopsyTurvy <102550358+AutopsyTurvy@users.noreply.github.com>
2024-02-18 23:39:53 +0100

    could not get stuff to work yet- no worries! Next time! xxx

--- scriptFiles/product.js
+++ scriptFiles/product.js
@@ -1,12 +1,40 @@
 
-import { baseURL } from './urlCall.js';
 
-const productsURL = `${baseURL}/wp-json/wp/v2/media`;
+
+// Product.js
+
+
+
+import { productsURL } from './urlCall.js';
+
 
 fetch(productsURL)
   .then(response => response.json())
   .then(data => {
-    console.log(data); 
+    const container = document.getElementById('thumbnail-image');
+
+    data.forEach(product => {
+
+      const anchor = document.createElement('a');
+      
+      anchor.href = `/sitePages/product-details.html?id=${product.id}`;
+      
+  
+      const img = document.createElement('img');
+      img.src = product.source_url;
+      img.alt = product.alt_text || 'Product image';
+      img.style.border = '1px solid rgba(59, 53, 48, 0.5)';
+      img.style.margin = '1em';
+      img.style.borderRadius = '50%';
    
+
+
+      anchor.appendChild(img);
+      
+ 
+      container.appendChild(anchor);
+    });
   })
-  .catch(error => console.error('Error fetching the images:', error));
+  .catch(error => console.error('Could not fetch the product images:', error));
+
+
--- scriptFiles/urlCall.js
+++ scriptFiles/urlCall.js
@@ -6,5 +6,6 @@
 const baseURL = "https://www.the-lore-of-pour.com";
 const allPostsURL = `${baseURL}/wp-json/wp/v2/posts?per_page=6&_embed`;
 const cardPostsURL = `${baseURL}/wp-json/wp/v2/posts?per_page=20&_embed`;
+const productsURL = `${baseURL}/wp-json/wp/v2/media?per_page=24&_embed`;
 
-export { baseURL, allPostsURL, cardPostsURL };
+export { baseURL, allPostsURL, cardPostsURL, productsURL };
